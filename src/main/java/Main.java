a88ebf993bafc8511995e00f45f31097daf4f4a8
Fakhrussyakirin <akkeeer702@gmail.com>
2022-07-04 11:34:51 +0700

    Read, Search, Delete pada KabupatenViewFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import helpers.Koneksi;
 
 public class Main {
     public static void main(String[] args) {
-        Koneksi.getConnection();
-    //    KabupatenViewFrame viewFrame = new KabupatenViewFrame();
-    //    viewFrame.setVisible(true);
+    //    Koneksi.getConnection();
+        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        viewFrame.setVisible(true);
     }
 }
